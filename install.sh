38d89ed1f56a1a4757cf05e4c24494c725e7420c
Nic Hannah <nic@doubleprecision.com.au>
2021-09-06 13:33:03 +1000

    Improve install script.

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.9.2-Linux-x86_64.sh && \
 sh Miniconda3-py39_4.9.2-Linux-x86_64.sh -b -p $(pwd)/env && \
 
+export PATH=$(pwd)/env/bin:$PATH
 ./env/bin/conda install -y mamba -c conda-forge && \
 ./env/bin/mamba env create --file conda_env.yaml && \
 
